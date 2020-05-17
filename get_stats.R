5e248be2611604bb2ba202c137b84612c6c7fb8a
Paul Tanger <detroitstylz@gmail.com>
2020-05-17 17:26:13 -0600

    move stats to separate script

---
+++ get_stats.R
@@ -0,0 +1,36 @@
+
+# get quick stats on dates..
+# so we want to get the JHU peak date..
+# keep what we need
+PeakMedAllCntryAllScensCases = subset.alls.plot[,c("age","t","scen_id","Date","date_50"):=NULL]
+
+# get peaks
+PeakMedAllCntryAllScensCases2 = PeakMedAllCntryAllScensCases[
+  PeakMedAllCntryAllScensCases[, .I[which.max(med)], by=list(Country_OU, compartment,Scenarios)]$V1]
+
+# drop NA
+summary(PeakMedAllCntryAllScensCases2)
+PeakMedAllCntryAllScensCases2 = na.omit(PeakMedAllCntryAllScensCases2, cols="Date_JHU")
+# save this in case want to use pivot tables
+filename = addStampToFilename("PeakMedAllCntryAllScensCases2", "csv")
+write.csv(PeakMedAllCntryAllScensCases2, filename, row.names = F)
+
+# instead of just cases below, just get the ranges of everything
+PeakMedAllCntryAllScens.Ranges.n.etc = PeakMedAllCntryAllScensCases2[, .( Min=min(Date_JHU, na.rm=T), Median=median(Date_JHU), Max=max(Date_JHU, na.rm=T)), by=c('Continent', 'USAID_region', 'compartment', 'Scenarios')]
+
+PeakMedAllCntryAllScens.Ranges.n.etc = setDT(PeakMedAllCntryAllScensCases2)[, .(.N, Min=min(Date_JHU, na.rm=T), Median=median(Date_JHU), Max=max(Date_JHU, na.rm=T)), by=c('Continent', 'USAID_region', 'compartment', 'Scenarios')]
+
+# just cases
+PeakMedAllCntryAllScensCases2 = PeakMedAllCntryAllScensCases2[compartment == "cases"]
+
+# so we want the range of dates for each scenario (for all countries)
+# maybe later do for USAID countries
+summary(PeakMedAllCntryAllScensCases2$Date_JHU)
+with(PeakMedAllCntryAllScensCases2, tapply(Date_JHU, Scenarios, range))
+
+filename = addStampToFilename("DateStats_AllCountries", "txt")
+setwd(datadir)
+sink(filename)
+with(PeakMedAllCntryAllScensCases2, tapply(Date_JHU, Scenarios, range))
+sink()
+
