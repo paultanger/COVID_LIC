788b8bac4260eb9e8de01567952399253a6c79e5
Paul Tanger <detroitstylz@gmail.com>
2020-06-02 15:05:17 -0600

    calc peak date ranges for regions, countries etc

--- get_stats.R
+++ get_stats.R
@@ -6,15 +6,123 @@ source('functions.R')
 source('plot_function.R')
 source('load_libs.R')
 #setwd("2020_05_05_archived_04_30_generated/afghanistan/")
+setwd(datadir)
+
+subset.alls.plot = readRDS("subset.alls.plot_20200527_1159.RObj")
+compartments = c("cases", "death_o")
+countries = levels(subset.alls.plot$LSHTM_Country)
+AllAllsData.7scens.AgeAll.SelectCountries = subset.alls.plot[LSHTM_Country %in% countries & compartment %in% compartments]
+AllAllsData.7scens.AgeAll.SelectCountries = droplevels(AllAllsData.7scens.AgeAll.SelectCountries)
+PeakMedAllCntryAllScensCases = as.data.table(AllAllsData.7scens.AgeAll.SelectCountries)
+as.data.frame(colnames(PeakMedAllCntryAllScensCases))
+PeakMedAllCntryAllScensCases = PeakMedAllCntryAllScensCases[,c(2,7,33,42,45,38)]
+head(PeakMedAllCntryAllScensCases, 2)
 
 # get quick stats on dates..
 # so we want to get the JHU peak date..
 # keep what we need
-PeakMedAllCntryAllScensCases = subset.alls.plot[,c("age","t","scen_id","Date","date_50"):=NULL]
+#PeakMedAllCntryAllScensCases = subset.alls.plot[,c("age","t","scen_id","Date","date_50"):=NULL]
 
 # get peaks
 PeakMedAllCntryAllScensCases2 = PeakMedAllCntryAllScensCases[
-  PeakMedAllCntryAllScensCases[, .I[which.max(med)], by=list(Country_OU, compartment,Scenarios)]$V1]
+  PeakMedAllCntryAllScensCases[, .I[which.max(med)], by=list(USAID_Country, compartment, Scenarios)]$V1]
+
+# drop med, we just want dates
+PeakMedAllCntryAllScensCases2 = PeakMedAllCntryAllScensCases2[,-6]
+
+# drop countries with no data
+PeakMedAllCntryAllScensCases2 = PeakMedAllCntryAllScensCases2[!is.na(PeakMedAllCntryAllScensCases2$Date_JHU),]
+
+# can do it with dt functions?
+# PeakMedAllCntryAllScensCases3 = PeakMedAllCntryAllScensCases2[, peak_diff := compartment.Date_JHU - compartment.Date_JHU, keyby=.(USAID_Country, region_abbrev, Scenarios)]
+# PeakMedAllCntryAllScensCases3 = PeakMedAllCntryAllScensCases2[PeakMedAllCntryAllScensCases2, on=.(Date_JHU, compartment), peak_diff := Date_JHU - i.Date_JHU, by=.(USAID_Country, region_abbrev, Scenarios)]
+# PeakMedAllCntryAllScensCases3 = PeakMedAllCntryAllScensCases2[, peak_diff := Date_JHU - Date_JHU, by=.(USAID_Country, region_abbrev, Scenarios)]
+# 
+# data[data[condition=="dark"], on=.(measure,subject), v := score - i.score ]
+
+# get range of peak death days
+peak_death_dates = PeakMedAllCntryAllScensCases2[compartment == "death_o"]
+deaths.summary = setDT(peak_death_dates)[, .(.N, Min=min(Date_JHU),
+                                               Median=median(Date_JHU),
+                                               Max=max(Date_JHU)), 
+                                           keyby=.(Scenarios)]
+
+deaths.summary.region = setDT(peak_death_dates)[, .(.N, Min=min(Date_JHU),
+                                             Median=median(Date_JHU),
+                                             Max=max(Date_JHU)), 
+                                         keyby=.(Scenarios, region_abbrev)]
+
+# save these
+setwd(datadir)
+filename = addStampToFilename("peak_death_dates", "csv")
+write.csv(deaths.summary, filename, row.names = F)
+
+setwd(datadir)
+filename = addStampToFilename("deaths.summary.region", "csv")
+write.csv(deaths.summary.region, filename, row.names = F)
+
+# make wide to calc day diff
+PeaksbyCountry.wide = dcast.data.table(PeakMedAllCntryAllScensCases2, USAID_Country + region_abbrev ~ Scenarios + compartment, value.var = "Date_JHU")
+
+# save this
+setwd(datadir)
+filename = addStampToFilename("PeakCaseDeathDatesAllCountries", "csv")
+write.csv(PeaksbyCountry.wide, filename, row.names = F)
+
+# get diffs the long way..
+PeaksbyCountry.wide$unmit_diff = PeaksbyCountry.wide$Unmitigated_death_o - PeaksbyCountry.wide$Unmitigated_cases
+PeaksbyCountry.wide$dist20_diff = PeaksbyCountry.wide$`20% distancing_death_o` - PeaksbyCountry.wide$`20% distancing_cases`
+PeaksbyCountry.wide$dist50_diff = PeaksbyCountry.wide$`50% distancing_death_o` - PeaksbyCountry.wide$`50% distancing_cases`
+PeaksbyCountry.wide$shield4080_diff = PeaksbyCountry.wide$`40/80 shielding_death_o` - PeaksbyCountry.wide$`40/80 shielding_cases`
+PeaksbyCountry.wide$shield8080_diff = PeaksbyCountry.wide$`80/80 shielding_death_o` - PeaksbyCountry.wide$`80/80 shielding_cases`
+PeaksbyCountry.wide$shield80dist20_diff = PeaksbyCountry.wide$`80/80 shielding, 20% distancing_death_o` - PeaksbyCountry.wide$`80/80 shielding, 20% distancing_cases`
+PeaksbyCountry.wide$shield80dist50_diff = PeaksbyCountry.wide$`80/80 shielding, 50% distancing_death_o` - PeaksbyCountry.wide$`80/80 shielding, 50% distancing_cases`
+
+# just keep diffs
+as.data.frame(colnames(PeaksbyCountry.wide))
+PeaksbyCountry.wide = PeaksbyCountry.wide[,c(1,2,17:23)]
+head(PeaksbyCountry.wide)
+# make long
+PeaksbyCountry.long = melt(PeaksbyCountry.wide, id = c("USAID_Country", "region_abbrev"), variable.name = "Scenarios", value.name = "days_between_peaks")
+
+PeaksbyCountryRegion.summary = setDT(PeaksbyCountry.long)[, .(Min=min(days_between_peaks),
+                                                       Median=median(days_between_peaks),
+                                                       Max=max(days_between_peaks)), 
+                                                   keyby=.(Scenarios, region_abbrev, USAID_Country)]
+
+PeaksbyRegion.summary = setDT(PeaksbyCountry.long)[, .(Min=min(days_between_peaks),
+                               Median=median(days_between_peaks),
+                               Max=max(days_between_peaks)), 
+                               keyby=.(Scenarios, region_abbrev)]
+
+Peaks.summary = setDT(PeaksbyCountry.long)[, .(Min=min(days_between_peaks),
+                                                        Median=median(days_between_peaks),
+                                                        Max=max(days_between_peaks)), 
+                                                    keyby=.(Scenarios)]
+
+Peaks.summary$diff = Peaks.summary$Max - Peaks.summary$Min
+
+PeaksbyCountry.range = Peaks.summary[,c(1,2,4)]
+
+# save these
+#PeaksbyCountryRegion.summary
+filename = addStampToFilename("PeaksbyCountryRegion.summary", "csv")
+write.csv(PeaksbyCountryRegion.summary, filename, row.names = F)
+
+#PeaksbyRegion.summary
+filename = addStampToFilename("PeaksbyRegion.summary", "csv")
+write.csv(PeaksbyRegion.summary, filename, row.names = F)
+
+#Peaks.summary
+filename = addStampToFilename("Peaks.summary", "csv")
+write.csv(Peaks.summary, filename, row.names = F)
+
+#PeaksbyCountry.range
+filename = addStampToFilename("PeaksbyCountry.range", "csv")
+write.csv(PeaksbyCountry.range, filename, row.names = F)
+
+# load the file above
+setwd(datadir)
 
 # get min and max too
 MedPeakMedAllCntryAllScensCases = PeakMedAllCntryAllScensCases[
@@ -104,3 +212,30 @@ sink(filename)
 with(PeakMedAllCntryAllScensCases2, tapply(Date_JHU, Scenarios, range))
 sink()
 
+# what is death rate?
+# load file
+subset.alls.plot = readRDS("subset.alls.plot_20200527_1159.RObj")
+compartments = c("cases", "death_o")
+countries = levels(subset.alls.plot$LSHTM_Country)
+AllAllsData.7scens.AgeAll.SelectCountries = subset.alls.plot[LSHTM_Country %in% countries & compartment %in% compartments]
+AllAllsData.7scens.AgeAll.SelectCountries = droplevels(AllAllsData.7scens.AgeAll.SelectCountries)
+countrieslist = split(AllAllsData.7scens.AgeAll.SelectCountries, by="USAID_Country")
+AllAllsData.7scens.AgeAll.SelectCountries$USAID_Country = as.factor(AllAllsData.7scens.AgeAll.SelectCountries$USAID_Country)
+countries = names(countrieslist)
+
+death.dt = as.data.table(AllAllsData.7scens.AgeAll.SelectCountries)
+death.dt = death.dt[,c(2,33,38,42)]
+# dcast
+death.dt.wide = dcast(death.dt, USAID_Country + Scenarios ~ compartment, value.var = "med", fun.aggregate = sum)
+death.dt.wide$death_rate = (death.dt.wide$death_o / death.dt.wide$cases) * 100
+# death.dt.wide = as.data.table(death.dt.wide)
+# calculate
+# death_rate = death.dt.wide[, as.list(summary(death.dt.wide$death_rate)), by=c('USAID_Country', 'Scenarios')]
+# death_rate <- aggregate(.~Scenarios, death.dt.wide, FUN=c(min, median, max))
+
+death.dt.wide = death.dt.wide[,c(1,2,5)]
+death_rate2 <- aggregate(death_rate ~ Scenarios, data=death.dt.wide, FUN=function(x) c(min(x), median(x), max(x)))
+death_rate2 <- cbind(death_rate2[,1], as.data.frame(death_rate2[,2]))
+names(death_rate2) <- c("Scenario", "min", "median", "max")
+
+# get diff between peak case and peak death day for each country and then get range
